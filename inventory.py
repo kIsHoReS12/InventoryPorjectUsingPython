a1a1e0ae1c1a42fd67c873a74c6daa32ce8976d6
Kishore Saravanan <167516361+kIsHoReS12@users.noreply.github.com>
2025-01-05 22:18:01 +0530

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,9 +1,9 @@
 import json
 
 class Inventory:
-    def __intit__(self,id,qt):
+    def __init__(self,id=None,qt=None):
         self.id = id
-        self.quantity = qt
+        self.qt = qt
         with open("stock.json","r") as file:
             stock_data = json.load(file)
             for stock in stock_data:
@@ -12,19 +12,20 @@ class Inventory:
                     self.stock = stock["stock"]
                     self.name = stock["name"]
 
-    def items_sell(self):
-            with open('stock.json', 'w') as file:
+    def items_sell(self,id=None,qt=None):
+            
+            with open('stock.json', 'r+') as file:
                 stock_data = json.load(file)
                 for stock in stock_data:
                     if stock["id"] == id:
                         stock["stock"] -= self.qt
+                file.seek(0)
                 json.dump(stock_data,file,indent=4)
-                file.close()
+                
     
     def display_items(self):
         with open("stock.json","r") as file:
             display_data = json.load(file)
             for data in display_data:
-                print(f"Name: {data["name"]} * AvaiableQt: {data["qt"]} * Price/Qt: {data["price"]}")
-
-
+                print(f"Name: {data['name']} * AvaiableQt: {data['stock']} * Price/Qt: {data['price']}")
+                
