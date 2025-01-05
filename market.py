454b4950aaad7d328389ec5d5fa8d07f88d2e33f
Kishore Saravanan <167516361+kIsHoReS12@users.noreply.github.com>
2025-01-05 20:58:38 +0530

    Update market.py

--- market.py
+++ market.py
@@ -1,23 +1,48 @@
 import inventory
+import json 
+
 class Market(inventory):
     def __init__(self):
         self.Confirm= 0
+        self.CurrentOrders = []
     
-    def check(self,item_id,item_qt):
+    def DisplayProduct(self):
+        inv = inventory()
+        inv.display_items()
+    
+    def CheckProduct(self,item_id,item_qt):
         inv = inventory(item_id,item_qt)
         if (inv.id>=1 and inv.id<=15):
             if (inv.stock >= inv.qt):
                 self.Confirm = 1
+                self.CurrentOrders.append([inv.name,inv.qt,inv.price])
             else:
+                self.CurrentOrders = []
                 if inv.stock == 0:print(f"{inv.name} is out of stock")
-                else:print(f"Please Enter your quantity with avaiable stock for {inv.name}")                                  #15 24 310
+                else:print(f"Please Enter your quantity with avaiable stock for {inv.name}")   
+                return                          
         else:
+            self.CurrentOrders = []
             print("Please Enter the correct Item ID!!")
-    def Sell(self,item_id,item_qt):
-        if self.check == 1:
+            return 
+    
+    def SellProduct(self,item_id,item_qt):
+        #if self.check == 1:
             inv = inventory(item_id,item_qt)
-            if (inv.id>=1 and inv.id<=15):
-                if (inv.stock >= inv.qt):
-                    inventory.item_sell(item_id,item_qt)
-                else:
-        print("")
+            inv.item_sell(item_id,item_qt)
+
+    
+    def BillDisplay(self):
+        print("Item:\t    Qt:\t    Price")
+        print()
+        totalprice = 0
+        for items in self.CurrentOrders:
+            print(f"{items[0]}\t -  {items[1]}\t  -  {items[1]*items[2]}")
+            totalprice+=items[1]*items[2]
+        print(f"\t\tTOTAL PRICE = RS {totalprice}")
+        with open("proft.json","w") as file:
+            profit = json.load(file)
+            profit["profit"] += totalprice
+        print("Thank you, Please visit again !")
+        
+        
